16edc7bff295bbda8ad6aac6d23193226a86152f
enikebraimoh <richardbraimoh@gmail.com>
2021-08-19 11:05:44 +0100

    done with the forms

--- main.go
+++ main.go
@@ -13,7 +13,29 @@ func init() {
 
 func main() {
 	http.HandleFunc("/", index)
+	http.HandleFunc("/process", process)
 	http.ListenAndServe(":8080", nil)
+
+}
+
+func process(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "GET" {
+		http.Redirect(w, r, "/", http.StatusSeeOther)
+		return
+	}
+
+	fname := r.FormValue("firster")
+	lname := r.FormValue("laster")
+
+	d := struct {
+		First string
+		Last  string
+	}{
+		First: fname,
+		Last:  lname,
+	}
+
+	tpl.ExecuteTemplate(w, "process.gohtml", d)
 
 }
 
